00dbf17340777ac2f65446ad74b31da112464ae2
Adithya Visnu <adithyavisnu@gmail.com>
2022-09-01 22:42:47 +0700

    update triangle untuk berdasarkan ganjil

--- loop-triangle-test.js
+++ loop-triangle-test.js
@@ -1,5 +1,5 @@
-let triangle = 9;
-let indexspasi = 9-1;
+let triangle = 11;
+let indexspasi = triangle-1;
 for (let index = 0; index <= triangle; index++) {
     for (let indexspasi = triangle-(index+1); indexspasi >= 0; indexspasi--) {
         process.stdout.write(" ");
