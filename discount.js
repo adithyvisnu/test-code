137b6436a59e333445ba8604f7228b40fe4651cb
Adithya Visnu <adithyavisnu@gmail.com>
2022-09-11 00:11:58 +0700

    update test discount

---
+++ discount.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        product: 'Tempe Goreng',
+        buy: 1,
+        price: 500
+    },
+    {
+        product: 'Tempe Goreng',
+        buy: 5,
+        price: 2300
+    },
+    {
+        product: 'Tempe Goreng',
+        buy: 10,
+        price: 4500
+    }
+];
+
+
+console.log(data);
