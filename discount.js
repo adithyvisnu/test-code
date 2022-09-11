5b393d9dc5ae52a55f81af7163adfcdf58ed3a0e
Adithya Visnu <adithyavisnu@gmail.com>
2022-09-11 21:46:25 +0700

    update solve problem

--- discount.js
+++ discount.js
@@ -16,5 +16,26 @@ const data = [
     }
 ];
 
+const beli = [
+    {
+        product: 'Tempe Goreng',
+        qty: 23
+    }
+]
 
-console.log(data);
+let totalHarga = 0;
+
+for (let indexBeli = 0; indexBeli < beli.length; indexBeli++) {
+    const filterBarang = data.filter((val) => val.product === beli[indexBeli].product).sort((a, b) => b.buy - a.buy);
+    let totalHargaProduk = 0;
+    let sisaQty = beli[indexBeli].qty;
+    for (let index = 0; index < filterBarang.length; index++) {
+        const applyTimes = Math.floor(sisaQty / filterBarang[index].buy);
+        totalHargaProduk += applyTimes * filterBarang[index].price;
+        sisaQty = sisaQty - (applyTimes * filterBarang[index].buy);
+    }
+    totalHarga += totalHargaProduk;
+}
+
+console.log('Total biaya setelah promo harga: ')
+console.log(totalHarga);
